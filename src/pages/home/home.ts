81120ffe1bad437a03961b6eee9fd4d10a0a7524
Moath Saleh <moad101@gmail.com>
2017-03-14 08:51:21 +0300

    To Do app static page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,62 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    tasks: any[];
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+    constructor(private alertCtrl: AlertController) {
+        this.tasks = [
+            {task: 'task1', priority: 'low', status: 'pending'},
+            {task: 'task2', priority: 'high', status: 'pending'},
+            {task: 'task3', priority: 'medium', status: 'priority'},
+            {task: 'task4', priority: 'done', status: 'done'},
+        ];
+    }
+
+    createTask() {
+        let alert = this.alertCtrl.create({
+            title: 'Login',
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'task name'
+                },
+                {
+                    name: 'details',
+                    placeholder: 'task details'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Create',
+                    handler: data => {
+                        console.log('Create clicked');
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    editTask(task) {
+        // edit task
+    }
+
+    deleteTask(task) {
+        // delete task
+    }
+
 
 }
